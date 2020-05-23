5f834dca35dabffb4a7fc62799cb83d70d75f315
Elismar13 <elismarsilva.5@outlook.com>
2020-05-23 13:00:26 -0300

    :tada: init

---
+++ 2D PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float jumpForce = 10f;
+    private bool leftMovement; 
+
+    private bool isGrounded;
+    private bool isJumping;
+    public Animator animation;
+    public Transform feetPos;
+    public LayerMask whatIsGround;
+    public float checkRadius;
+
+    private Rigidbody2D player;
+
+    void Awake() {
+        player = GetComponent<Rigidbody2D>();
+        this.leftMovement = true;
+    }
+
+    void Update() {
+        isGrounded = Physics2D.OverlapCircle( feetPos.position, checkRadius, whatIsGround );
+
+        if (isGrounded)
+            isJumping = false;
+
+        if (!isJumping && Input.GetKey(KeyCode.Space)) {
+            isJumping = true;
+            player.velocity = Vector2.up * jumpForce;
+        }
+
+    }
+
+    void FixedUpdate() {
+        float horizontalMovement = Input.GetAxisRaw("Horizontal") * moveSpeed;
+
+        if ((horizontalMovement > 0 && !leftMovement) || (horizontalMovement < 0 && leftMovement))
+            flipPlayer();
+
+        animation.SetFloat("Speed", Mathf.Abs(horizontalMovement));
+        player.velocity = new Vector2( horizontalMovement, player.velocity.y );
+    }
+
+    private void flipPlayer( ) {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+
+        this.leftMovement = !this.leftMovement;
+    }
+}
