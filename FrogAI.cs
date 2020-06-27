b964b89f9179bcc5e76befefcaea0fff8f3c9c43
Elismar13 <elismarsilva.5@outlook.com>
2020-06-27 14:10:25 -0300

    :pencil: Adding time routine

--- FrogAI.cs
+++ FrogAI.cs
@@ -9,33 +9,43 @@ public class FrogAI : MonoBehaviour
     //This is the player object
     public Transform player;
 
-    public float jumpSpeed;
-    public float movementSpeed;
+    [Range(0, 10f)]public float jumpSpeed;
+    [Range(0, 10f)]public float frogMovement;
+    [Range(0, 10f)]public float frogSpeed;
 
     
-
     private void Start() {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         frog = this.gameObject.GetComponent<Rigidbody2D>();
-    }
 
-    private void Update() {
-        this.moveFrog();
+        StartCoroutine(frogController());
     }
 
     void moveFrog() {
+
         float playerPositionX = player.position.x;
         float frogPositionX = this.frog.velocity.x;
         float playerEnemyDiference = playerPositionX - frogPositionX;
         
-        Debug.Log(playerEnemyDiference);
+        //Debug.Log(playerEnemyDiference);
 
         if(playerEnemyDiference > 0) {
-            frog.velocity = new Vector2(frogPositionX + movementSpeed, this.frog.position.y);
+            frog.velocity = new Vector2(frogPositionX + frogMovement * frogSpeed, this.frog.position.y);
         }
         else if (playerEnemyDiference < 0){
-            frog.velocity = new Vector2(frogPositionX - movementSpeed, this.frog.position.y);
+            frog.velocity = new Vector2(frogPositionX - frogMovement * frogSpeed, this.frog.position.y);
         }
     }
+
+    // move frog in a x secounds interval
+    IEnumerator frogController() {
+        moveFrog();
+
+        // force system to wait x secounds
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(frogController());
+    }
 }
+
+
  
