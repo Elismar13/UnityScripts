b6a585865223f783c41c2c17d60344424419542e
Elismar13 <elismarsilva.5@outlook.com>
2020-06-28 22:00:13 -0300

    :pencil: New player controller script

---
+++ PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+
+
+public class PlayerController : MonoBehaviour
+{
+    [Range(0, 100)] public int life;
+
+    public Text DisplayLife;
+    void Start() {
+        DisplayLife.supportRichText = false;
+    }
+
+    void Update() {
+        
+    }
+
+    public void takeDamage(int damage) {
+        this.life -= damage;
+    }
+    private void LateUpdate() {
+        DisplayLife.text = string.Format("Life: {0}", life);
+    }
+}
