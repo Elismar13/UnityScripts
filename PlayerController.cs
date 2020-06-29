4e67bd6322760ff59b960055919e60bb84149b11
Elismar13 <elismarsilva.5@outlook.com>
2020-06-29 11:53:24 -0300

    :snake: Collider detection Trigger

--- PlayerController.cs
+++ PlayerController.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-
 public class PlayerController : MonoBehaviour
 {
     [Range(0, 100)] public int life;
 
     public Text DisplayLife;
+
+    private bool damage = false;
     void Start() {
         DisplayLife.supportRichText = false;
     }
@@ -19,10 +20,27 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    public void takeDamage(int damage) {
-        this.life -= damage;
-    }
     private void LateUpdate() {
         DisplayLife.text = string.Format("Life: {0}", life);
+    }
+
+    //Checking if player collider with the frog
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Frog")) {
+            takeDamage(5);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision) {
+        foreach (ContactPoint2D contact in collision.contacts) {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+    }
+
+    public void takeDamage(int damage) {
+        this.life -= damage;
     }
 }
