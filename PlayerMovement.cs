4c2ec3edfe645a4f65a5312d1ee541372a1afdf5
Elismar13 <elismarsilva.5@outlook.com>
2020-07-16 17:52:33 -0300

    Crouch action detection script

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -9,26 +9,29 @@ public class PlayerMovement : MonoBehaviour
     [Range(0, 100f)] public float moveSpeed = 10f;
     [Range(0, 100f)] public float jumpForce = 10f;
     private bool leftMovement; 
-
     private bool isGrounded;
     private bool isJumping;
+    private bool isCrouched;
+    
     public Animator PlayerAnimation;
     public Transform feetPos;
     public LayerMask whatIsGround;
     public float checkRadius;
 
+    public GameObject Header;
     private Rigidbody2D player;
 
     void Awake() {
         player = GetComponent<Rigidbody2D>();
         this.leftMovement = true;
+        this.isJumping = false;
+        this.isCrouched = false;
     }
 
     void Update() {
         isGrounded = Physics2D.OverlapCircle( feetPos.position, checkRadius, whatIsGround );
 
-        if (isGrounded)
-        {
+        if (isGrounded) {
             isJumping = false;
             PlayerAnimation.SetBool("isJumping", false);
         }
@@ -43,9 +46,14 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate() {
         float horizontalMovement = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        float verticalMovement = Input.GetAxisRaw("Vertical");
 
         if ((horizontalMovement > 0 && !leftMovement) || (horizontalMovement < 0 && leftMovement))
             flipPlayer();
+
+        if(verticalMovement < 0) {
+
+        }
 
         PlayerAnimation.SetFloat("Speed", Mathf.Abs(horizontalMovement));
         player.velocity = new Vector2( horizontalMovement, player.velocity.y );
