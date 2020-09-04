31b8b74bf6952043bf41e0dddae9b3587614d12b
Elismar13 <elismarsilva.5@outlook.com>
2020-09-04 15:29:07 -0300

    Player health, max health and boost lenght

---
+++ Estellar/Player/PlayerStats.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+    public int life;
+    public int maxLife;
+    public int jetpackBooster;
+
+    public HealthBar healthBar;
+
+    private void Start() {
+        healthBar.setHealth(maxLife);
+    }
+    public void takeDamage(int damage) {
+        this.life -= damage;
+        healthBar.setHealth(life);
+    }
+}
