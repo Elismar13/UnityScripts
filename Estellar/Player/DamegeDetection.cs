b510a4b157625421516221f8ae27291a5d9bbf72
Elismar13 <elismarsilva.5@outlook.com>
2020-09-04 15:28:11 -0300

    Damage controller script

---
+++ Estellar/Player/DamegeDetection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamegeDetection : MonoBehaviour {
+
+    [Range(0, 10)] public int spikeDamage;
+    private GameObject player;
+
+    private void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Spikes")) {
+            print("hero");
+            player.GetComponent<PlayerStats>().takeDamage(spikeDamage);
+        }
+    }
+}
