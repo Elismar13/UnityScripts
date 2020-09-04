a2e8e8448728538917edde040df642614a0077b2
Elismar13 <elismarsilva.5@outlook.com>
2020-09-04 15:27:43 -0300

    AnimationController

---
+++ Estellar/Player/AnimationController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    public Rigidbody2D player;
+
+    public Animator PlayerAnimation;
+
+    private bool leftMovement = true; 
+
+    private bool isGrounded;
+
+    private bool isJumping;
+
+    void Start() {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        // getting data from Movement class
+        isGrounded = player.GetComponent<Movement>().getIsGrounded();
+        isJumping = player.GetComponent<Movement>().getIsJumping();
+
+        if (isGrounded) {
+            PlayerAnimation.SetBool("isJumping", false);
+        }
+
+        if (isJumping) {
+            PlayerAnimation.SetBool("isJumping", true);
+        }
+    }
+
+    private void FixedUpdate() {
+        Vector2 velocity = player.velocity;
+        if ((velocity.x > 0 && !leftMovement) || (velocity.x < 0 && leftMovement))
+            flipPlayer();
+
+        PlayerAnimation.SetFloat("Speed", Mathf.Abs(velocity.x));
+    }
+
+    private void flipPlayer( ) {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+
+        this.leftMovement = !this.leftMovement;
+    }
+}
