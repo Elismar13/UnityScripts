bfa44bf80be65e1897afa2368bf17b793e3866a5
Elismar13 <elismarsilva.5@outlook.com>
2020-09-08 18:41:44 -0300

    background will colapse and transferred to forward after ends

---
+++ Estellar/Camera/Parallax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float lenght;
+    private float startpos;
+    private GameObject cam;
+
+    private Transform player;
+    public float parallax;
+    [Range(0, 100f)] public float replaceBackgroundXAxis;
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+        startpos = transform.position.x;
+        lenght = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void FixedUpdate() {
+        float distance = cam.transform.position.x * parallax;
+
+        transform.position = new Vector3(startpos + distance,
+                                        this.transform.position.y,
+                                        transform.position.z);
+
+        print(String.Format("{0} - {1} && {2} ", player.position.x, this.transform.position.x,  lenght));
+        if(player.position.x - this.transform.position.x >= lenght) {
+            this.transform.position = player.position;
+        }
+    }
+}
