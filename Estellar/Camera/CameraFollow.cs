ec1a42485b7ae810527c6df853b8edfb92eaef6d
Elismar13 <elismarsilva.5@outlook.com>
2020-09-08 18:40:59 -0300

    Camera will behave different when jumping

---
+++ Estellar/Camera/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    [Range(0, 10f)] public float cameraSmoothness;
+
+    private float followPlayerHeightOffset;
+    private Vector3 targetOffset;
+    private GameObject player;
+
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+        targetOffset = this.transform.position - player.transform.position;
+        followPlayerHeightOffset = 0;
+    }
+
+    void FixedUpdate() {
+        bool isJumping = player.GetComponent<Movement>()
+                                    .getIsJumping();
+        followPlayerHeightOffset = isJumping ? -0.5f : 0;
+        targetOffset.y += followPlayerHeightOffset;
+
+        transform.position = Vector3.Lerp(this.transform.position,
+                                            player.transform.position+targetOffset,
+                                            cameraSmoothness);
+        targetOffset.y -= followPlayerHeightOffset;
+    }
+}
